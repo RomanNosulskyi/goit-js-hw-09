f43590c00a1a1e8e500f827e210ef7088095b0f9
RomanNosulskyi <114739887+RomanNosulskyi@users.noreply.github.com>
2023-03-30 10:59:14 +0000

    Deploying to gh-pages from @ RomanNosulskyi/goit-js-hw-09@d4b5ea33fb34f559f4e5ea6394cc88b42ca6825b 🚀

---
+++ 01-color-switcher.9dacdc6f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled"),e.setAttribute("disabled","")}));var n=null}();
+//# sourceMappingURL=01-color-switcher.9dacdc6f.js.map
---
+++ 01-color-switcher.c89723b0.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled","")}));let r=null;
+//# sourceMappingURL=01-color-switcher.c89723b0.js.map
